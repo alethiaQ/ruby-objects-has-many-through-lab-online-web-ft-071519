069cf731ffa93e4135cbe52d662c51a0fb16bd23
Alethia Quintero <alethiaq22@gmail.com>
2019-07-30 19:45:03 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,8 +11,14 @@ class Artist
     @@all
   end
 
+  def new_song(name, genre)
+    song = Song.new(name, genre)
+    song.artist = self
+  end
 
-
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
 
 
 end
