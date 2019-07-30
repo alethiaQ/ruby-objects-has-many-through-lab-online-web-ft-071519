fd8e0917c9bdb325db17c82778d9d7be9b9b600a
Alethia Quintero <alethiaq22@gmail.com>
2019-07-30 19:56:57 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,13 +1,12 @@
-class Doctor 
+class Doctor
   @@all = []
   attr_accessor :name
 
   def initialize(name)
-    @name = name 
+    @name = name
     @@all << self
-  end 
-
-  def self.all 
-    @@all 
   end
-    
+
+  def self.all
+    @@all
+  end
