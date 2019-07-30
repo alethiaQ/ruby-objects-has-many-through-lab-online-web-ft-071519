c64d5c67e5ee133d681b23c474d040c1af61b1a7
Alethia Quintero <alethiaq22@gmail.com>
2019-07-30 19:53:00 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -10,4 +10,12 @@ class Genre
   def self.all
     @@all
   end
+
+  def songs
+    Song.all.select {|song| song.genre == self}
+  end
+
+  def artists
+    self.songs.collect {|song| song.artist }
+  end 
 end
