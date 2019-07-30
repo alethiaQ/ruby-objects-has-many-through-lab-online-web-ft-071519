a58bdebb4327c6d818d98129ef2134a0e440ce16
Alethia Quintero <alethiaq22@gmail.com>
2019-07-30 20:03:39 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -15,5 +15,12 @@ class Doctor
     appt = Appointment.new(date, patient, self)
   end
 
+  def appointments
+    Appointment.all.select {|appt| appt.doctor == self}
+  end
+
+  def patients
+    self.appointments.collect {|appt| appt.patients}
+  end
 
 end
